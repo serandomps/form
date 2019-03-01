4310a4b96a0d22107b7d8bdae619c82652c4b6a5
Ruchira Wageesha <ruchira.wageesha@gmail.com>
2019-03-01 06:54:53 +0530

    updating

--- index.js
+++ index.js
@@ -2,33 +2,45 @@ var serand = require('serand');
 var utils = require('utils');
 
 var sourcer = function (form, field) {
-    var selector = '.source[data-name=\'' + field + '\']';
-    return $(selector, form.el);
+    return $(form.elem).find('.source[data-name=\'' + field + '\']');
 };
 
-var findOne = function (form, field, done) {
-    var options = form.options[field];
-    if (!options || !options.find) {
+var findOne = function (form, name, done) {
+    var fields = form.fields[name];
+    if (!fields || !fields.find) {
         return done();
     }
-    var context = form.contexts[field];
-    var source = sourcer(form, field);
-    options.find(context, source, done);
+    var context = form.contexts[name];
+    var source = sourcer(form, name);
+    fields.find(context, source, done);
 };
 
-var validateOne = function (form, data, field, done) {
-    var options = form.options[field];
-    var value = data[field];
-    if (!options || !options.validate) {
+var validateOne = function (form, data, name, done) {
+    var fields = form.fields[name];
+    var value = data[name];
+    if (!fields || !fields.validate) {
         return done(null, null, value);
     }
-    var context = form.contexts[field];
-    options.validate(context, data, value, done);
+    var context = form.contexts[name];
+    fields.validate(context, data, value, done);
 };
 
-var Form = function (elem, options) {
+var onlyFields = function (o) {
+    var oo = {};
+    Object.keys(o).forEach(function (key) {
+        if (key === '_') {
+            return;
+        }
+        oo[key] = o[key];
+    });
+    return oo;
+};
+
+var Form = function (id, elem, options) {
+    this.id = id;
     this.elem = elem;
     this.options = options || {};
+    this.fields = onlyFields(options);
     this.contexts = {};
 };
 
@@ -38,9 +50,9 @@ Form.prototype.context = function (name, done) {
 
 Form.prototype.render = function (ctx, data, done) {
     var form = this;
-    var options = form.options;
-    async.each(Object.keys(options), function (field, eachDone) {
-        var o = options[field];
+    var fields = form.fields;
+    async.each(Object.keys(fields), function (field, eachDone) {
+        var o = fields[field];
         var render = o.render;
         if (!render) {
             return eachDone();
@@ -55,10 +67,10 @@ Form.prototype.render = function (ctx, data, done) {
 
 Form.prototype.create = function (data, done) {
     var form = this;
-    var options = form.options;
+    var fields = form.fields;
     var errors = {};
-    async.each(Object.keys(options), function (field, eachDone) {
-        var o = options[field];
+    async.each(Object.keys(fields), function (field, eachDone) {
+        var o = fields[field];
         var create = o.create;
         if (!create) {
             return eachDone();
@@ -88,7 +100,7 @@ Form.prototype.create = function (data, done) {
 Form.prototype.find = function (done) {
     var form = this;
     var data = {};
-    async.each(Object.keys(form.options), function (field, eachDone) {
+    async.each(Object.keys(form.fields), function (field, eachDone) {
         findOne(form, field, function (err, value) {
             if (err) {
                 return eachDone(err);
@@ -106,25 +118,41 @@ Form.prototype.find = function (done) {
 
 Form.prototype.validate = function (data, done) {
     var form = this;
-    var errors = {};
-    async.each(Object.keys(form.options), function (field, eachDone) {
-        validateOne(form, data, field, function (err, error, value) {
+    var fields = form.fields;
+    var options = form.options;
+    var validateFields = function () {
+        var errors = {};
+        async.each(Object.keys(fields), function (field, eachDone) {
+            validateOne(form, data, field, function (err, error, value) {
+                if (err) {
+                    return eachDone(err);
+                }
+                if (error) {
+                    errors[field] = error;
+                    return eachDone();
+                }
+                data[field] = value;
+                eachDone();
+            })
+        }, function (err) {
             if (err) {
-                return eachDone(err);
+                return done(err);
             }
-            if (error) {
-                errors[field] = error;
-                return eachDone();
-            }
-            data[field] = value;
-            eachDone();
-        })
-    }, function (err) {
+            errors = Object.keys(errors).length ? errors : null;
+            done(null, errors, data);
+        });
+    };
+    if (!options._ || !options._.validate) {
+        return validateFields();
+    }
+    options._.validate(data, function (err, errors, data) {
         if (err) {
             return done(err);
         }
-        errors = Object.keys(errors).length ? errors : null;
-        done(null, errors, data);
+        if (errors) {
+            return done(null, errors, data);
+        }
+        validateFields();
     });
 };
 
@@ -134,30 +162,29 @@ Form.prototype.update = function (errors, data, done) {
     var fields = Object.keys(data);
     errors = errors || {};
     data = data || {};
-    $('.source', form.elem).removeClass('u-has-error')
-        .removeClass('u-has-success')
+    $(form.elem).find('.source').removeClass('is-invalid')
+        .removeClass('is-valid')
         .find('.invalid-feedback')
         .html('')
         .addClass('hidden');
-    async.each(fields, function (field, eachDone) {
-        var error = errors[field];
-        var value = data[field];
-        var options = form.options[field];
-        if (!options || !options.update) {
+    async.each(fields, function (name, eachDone) {
+        var error = errors[name];
+        var value = data[name];
+        var fields = form.fields[name];
+        if (!fields || !fields.update) {
             return eachDone();
         }
-        var source = sourcer(form, field);
-        var context = form.contexts[field];
-        options.update(context, source, error, value, eachDone);
+        var source = sourcer(form, name);
+        var context = form.contexts[name];
+        fields.update(context, source, error, value, eachDone);
     }, function (err) {
-        console.log(errors)
         if (err) {
             return done(err);
         }
         var el;
         var field;
         var error;
-        var errored = false;
+        var errored;
         for (field in errors) {
             if (!errors.hasOwnProperty(field)) {
                 continue;
@@ -168,7 +195,7 @@ Form.prototype.update = function (errors, data, done) {
                 continue;
             }
             errored = true;
-            el.addClass('u-has-error')
+            el.addClass('is-invalid')
                 .find('.invalid-feedback')
                 .html(error)
                 .removeClass('hidden');
@@ -184,7 +211,7 @@ Form.prototype.update = function (errors, data, done) {
                 continue;
             }
             el = sourcer(form, field);
-            el.addClass('u-has-success');
+            el.addClass('is-valid');
         }
         done();
     });
@@ -194,20 +221,20 @@ Form.prototype.refresh = function (data, done) {
     var form = this;
     var fields = Object.keys(data);
     data = data || {};
-    async.each(fields, function (field, eachDone) {
-        var value = data[field];
-        var options = form.options[field];
-        if (!options || !options.update) {
+    async.each(fields, function (name, eachDone) {
+        var value = data[name];
+        var field = form.fields[name];
+        if (!field || !field.update) {
             return eachDone();
         }
-        var source = sourcer(form, field);
-        var context = form.contexts[field];
-        options.update(context, source, null, value, eachDone);
+        var source = sourcer(form, name);
+        var context = form.contexts[name];
+        field.update(context, source, null, value, eachDone);
     }, done);
 };
 
-module.exports.create = function (el, options) {
-    return new Form(el, options);
+module.exports.create = function (id, el, options) {
+    return new Form(id, el, options);
 };
 
 module.exports.selectize = function (elem, html) {
